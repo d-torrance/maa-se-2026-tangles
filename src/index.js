9c3a20bd6dcf3000fdcb963252e00b85f89420e8
Doug Torrance <dtorrance@piedmont.edu>
2026-03-16 13:57:52 -0400

    Switch to "moon" theme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import RevealMarkdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import RevealMath from "reveal.js/plugin/math/math.esm.js";
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
 import "reveal.js/dist/reveal.css";
-import "reveal.js/dist/theme/night.css";
+import "reveal.js/dist/theme/moon.css";
 import "reveal.js/plugin/highlight/zenburn.css"
 import macaulay2 from "highlightjs-macaulay2";
 
