630c3752c1237efdecd0dffa9f10f92e54207f37
Doug Torrance <dtorrance@piedmont.edu>
2026-03-16 13:56:47 -0400

    Add support for highlighting m2 code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import Reveal from "reveal.js";
 import RevealMarkdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import RevealMath from "reveal.js/plugin/math/math.esm.js";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/night.css";
+import "reveal.js/plugin/highlight/zenburn.css"
+import macaulay2 from "highlightjs-macaulay2";
 
 let deck = new Reveal({
-  plugins: [RevealMarkdown, RevealMath.KaTeX],
+  highlight: {
+      beforeHighlight: (hljs) => hljs.registerLanguage("macaulay2", macaulay2),
+  },
+    plugins: [RevealMarkdown, RevealMath.KaTeX, RevealHighlight],
 });
 deck.initialize();
